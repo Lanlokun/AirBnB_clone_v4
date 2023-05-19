e63dde9bf110e33c2dd9db6f724514c9614181de
malik <mlanlokun@gmail.com>
2023-05-19 03:47:42 +0300

    updates

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -2,8 +2,10 @@
 """ Starts a Flash Web Application """
 from models import storage
 from models.state import State
+from models.city import City
 from models.amenity import Amenity
 from models.place import Place
+from os import environ
 from flask import Flask, render_template
 import uuid
 app = Flask(__name__)
@@ -17,10 +19,12 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/1-hbnb', strict_slashes=False)
-def hbnb():
+@app.route('/2-hbnb', strict_slashes=False)
+def hbnb_filters(the_id=None):
     """ HBNB is alive! """
     states = storage.all(State).values()
+    for state in states:
+        print(state)
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
     cache_id = str(uuid.uuid4())
@@ -33,13 +37,13 @@ def hbnb():
 
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
+    print(places)
 
-    return render_template('1-hbnb.html',
+    return render_template('2-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
                            cache_id=cache_id)
-
 
 if __name__ == "__main__":
     """ Main Function """
